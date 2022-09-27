c5af0a893d6ee10988921c6a9ecb9324bd7d435d
xdxdxd <xdxdxd@xd>
2022-09-27 20:47:17 +0800

    取得輸入的名字，顯示在textview

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val etName = findViewById<EditText>(R.id.etName)
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        button.setOnClickListener{
+            textView.text=etName.text
+        }
     }
 }
